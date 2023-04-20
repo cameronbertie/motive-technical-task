b7a01e66ec59ecb57a57dd65d70e524c03a85be1
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2023-04-20 01:00:45 +0100

    Update Post.tsx

--- src/pages/components/Post.tsx
+++ src/pages/components/Post.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
-export default function Post({ post }: {post: any}) {
+export default function Post({ post }: {post?: any}) {
   const [expanded, setExpanded] = useState(false);
 
   const toggleExpanded = () => {
@@ -11,13 +11,23 @@ export default function Post({ post }: {post: any}) {
 
   return (
     <div className="post">
+      {post?.title && (
+        <div>
+          <h3>{post.title}</h3>
+          <hr />
+          {expanded ? <p>{post.body}</p> : null}
+        </div>
+      )}
       <div>
-        <h3>{post.title}</h3>
-        <hr />
-        {expanded ? <p>{post.body}</p> : null}
-      </div>
-      <div>
-        {expanded ? <button onClick={toggleExpanded}>Read Less<KeyboardArrowUpIcon /></button> : <button onClick={toggleExpanded}>Read More<KeyboardArrowRightIcon /></button>}
+        {expanded ? (
+          <button onClick={toggleExpanded}>
+            Read Less<KeyboardArrowUpIcon />
+          </button>
+        ) : (
+          <button onClick={toggleExpanded}>
+            Read More<KeyboardArrowRightIcon />
+          </button>
+        )}
       </div>
     </div>
   );
